f442e82cc627fb19c389f03df6b654c9509f9fd9
Vaibhav_28 <vaibhavkhadse838@gmail.com>
2025-02-27 10:01:01 +0530

    Db connection

--- src/index.js
+++ src/index.js
@@ -1,1 +1,33 @@
-ECHO is on.
+// require('dotenv').config({path:"./env"})
+
+import dotenv from 'dotenv'
+// import mongoose from "mongoose";
+// import { DB_Name } from "./constants";
+// import express from "express";
+import connectDB from "./db/DBconnect.js";
+
+dotenv.config({
+    path:"./"
+})
+
+connectDB();
+
+/*
+const app = express();
+(async()=>{
+    try {
+      await mongoose.connect(`${process.env.MONGODB_URI}/${DB_Name}`); 
+      app.on('error',()=>{
+      console.log("Error connecting",error);
+      throw error;
+      })
+     app.listen(process.env.POST, ()=>{
+        console.log(`Server is running on port ${process.env.PORT}`);
+     })
+
+    } catch (error) {
+        console.error(error);
+        throw error;
+    }
+})
+*/
